5292468c42555d6757f037380b3db07910546821
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-10-13 19:29:31 -0400

    Update alert_not_logged_in.php

--- includes/templates/alert_not_logged_in.php
+++ includes/templates/alert_not_logged_in.php
@@ -4,6 +4,6 @@ if(function_exists('ns_core_load_theme_options')) { $members_login_page = ns_cor
 ?>
 
 <div class="alert-box info">
-    <p><?php esc_html_e( 'You must be logged in to view this page.', 'ns-basics' ); ?></p>
+    <p><?php esc_html_e( 'You must be logged in and have the correct privileges to view this page.', 'ns-basics' ); ?></p>
     <?php if(!empty($members_login_page)) { ?><a class="button small" href="<?php echo $members_login_page; ?>"><?php esc_html_e( 'Login', 'ns-basics' ); ?></a><?php } ?>
 </div>
