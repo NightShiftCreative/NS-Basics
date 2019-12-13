d909747ca2f8700a970987b5df3b91de90f6ee6a
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-12-13 10:07:26 -0500

    Update wp-bakery.php

--- includes/wp-bakery.php
+++ includes/wp-bakery.php
@@ -36,6 +36,38 @@ function ns_basics_vc_map() {
 		),
 	));
 
+	/** LIST POSTS **/
+	vc_map(array(
+		'name' => esc_html__( 'List Posts', 'ns-basics' ),
+		'base' => 'ns_list_posts',
+		'description' => esc_html__( 'Display a list a posts', 'ns-basics' ),
+		'icon' => PROPERTYSHIFT_DIR.'/images/icon-real-estate.svg',
+		'class' => '',
+		'category' => 'NS Basics',
+		'params' => array(
+			array(
+				'type' => 'textfield',
+				'heading' => esc_html__( 'Number of Posts', 'ns-basics' ),
+				'param_name' => 'num',
+				'std' => 3,
+			),
+			array(
+				'type' => 'textfield',
+				'heading' => esc_html__( 'Excerpt Length', 'ns-basics' ),
+				'param_name' => 'excerpt',
+				'std' => 20,
+				'description' => esc_html__( 'Enter the number of characters to trim the excerpt to.', 'ns-basics' ),
+			),
+			array(
+				'type' => 'dropdown',
+				'heading' => esc_html__( 'Show Pagination', 'ns-basics' ),
+				'param_name' => 'show_pagination',
+				'value' => array('False' => 'false', 'True' => 'true'),
+				'std' => 'false',
+			),
+		),
+	));
+
 	/** LOGIN FORM **/
 	vc_map(array(
 		'name' => esc_html__( 'Login Form', 'ns-basics' ),
