b5c77d71a15d7b56f080b859327833dc5104887a
NightShiftCreative <40130800+NightShiftCreative@users.noreply.github.com>
2019-12-27 20:55:16 -0500

    Update class-ns-basics-admin.php

--- includes/classes/class-ns-basics-admin.php
+++ includes/classes/class-ns-basics-admin.php
@@ -738,7 +738,11 @@ class NS_Basics_Admin {
 		    	$settings[$key]['value'] = isset($values[$field['name']] ) ? $values[$field['name']] : $field['value'];
 		    	if(is_serialized($settings[$key]['value'][0])) { $settings[$key]['value'] = unserialize($settings[$key]['value'][0]); }
 		    } else {
-		    	if(isset($field['esc']) && $field['esc'] == false) { $field_value = $values[$field['name']][0]; } else { $field_value = esc_attr($values[$field['name']][0]); }
+		    	if(isset($field['esc']) && $field['esc'] == false) {   
+		    		$field_value = isset($values[$field['name']][0]) ? $values[$field['name']][0] : '';  
+		    	} else { 
+		    		$field_value = isset($values[$field['name']][0]) ? esc_attr($values[$field['name']][0]) : ''; 
+		    	}
 		    	$settings[$key]['value'] = isset( $values[$field['name']] ) ? $field_value : $field['value'];
 		    }
 		    
